63f3c2c673bd86abb4ca89b77c4bcfcd62df33cf
jfc3 <buckslice@gmail.com>
2018-01-26 22:21:15 -0800

    starting input manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    GameObject leftWire;
+    GameObject rightWire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+
+
+    }
+
+
+
+}
