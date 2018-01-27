87198ae69c961c3b37626165c47db026b26541f3
jfc3 <buckslice@gmail.com>
2018-01-27 00:01:37 -0800

    HINGEJOINTS

---
+++ Assets/Scripts/JointBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBuilder : MonoBehaviour {
+
+    public GameObject jointPrefab;
+
+    Rigidbody2D rigid;
+    PlayerController pc;
+
+
+    // Use this for initialization
+    void Start() {
+        rigid = GetComponent<Rigidbody2D>();
+        pc = GetComponent<PlayerController>();
+
+        int c = 10;
+        pc.leftWire = BuildJoints(c);
+        pc.rightWire = BuildJoints(c);
+
+
+    }
+
+    Rigidbody2D BuildJoints(int count) {
+        Rigidbody2D[] rbs = new Rigidbody2D[count];
+        for (int i = 0; i < count; ++i) {
+            GameObject go = Instantiate(jointPrefab, transform.position + new Vector3(i * 0.5f, 0, 0), Quaternion.identity, transform);
+            go.name = "Joint" + i;
+            HingeJoint2D joint = go.GetComponent<HingeJoint2D>();
+            Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
+            rbs[i] = rb;
+            if (i == 0) {
+                joint.connectedBody = rigid;
+            } else {
+                joint.limits = new JointAngleLimits2D { min = -90, max = 90 };
+                joint.connectedBody = rbs[i - 1];
+            }
+
+            if (i == count - 1) {
+                return rb;
+            }
+        }
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure; // Required in C#
 
 public class PlayerController : MonoBehaviour {
+    public PlayerIndex index;
+    [HideInInspector]
+    public Rigidbody2D leftWire;
+    [HideInInspector]
+    public Rigidbody2D rightWire;
 
-    GameObject leftWire;
-    GameObject rightWire;
+    public float armSpeed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
 
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+    }
 
+    // Update is called once per frame
+    void Update() {
+        float lx = GamePad.GetState(index).ThumbSticks.Left.X;
+        float ly = GamePad.GetState(index).ThumbSticks.Left.Y;
 
+        float rx = GamePad.GetState(index).ThumbSticks.Right.X;
+        float ry = GamePad.GetState(index).ThumbSticks.Right.Y;
+
+        leftWire.velocity = new Vector3(lx, ly) * armSpeed;
+        rightWire.velocity = new Vector3(rx, ry) * armSpeed;
+
+        //leftWire.AddForce(new Vector3(lx, ly)*armSpeed);
+        //rightWire.AddForce(new Vector3(rx, ry)*armSpeed);
+
+        //leftWire.transform.Translate(new Vector3(lx, ly) * Time.deltaTime);
+        //rightWire.transform.Translate(new Vector3(rx, ry) * Time.deltaTime);
 
     }
 
