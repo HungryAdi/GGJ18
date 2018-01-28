1a83af203421e62efaee778e3a165d503f35b4ef
Quinn <Qlevine@uci.edu>
2018-01-28 10:50:14 -0800

    The background lights up

---
+++ Assets/Scripts/LightUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUp : MonoBehaviour {
+    public bool litFam = false;
+    public Sprite unlit;
+    public Sprite lit;
+    private SpriteRenderer citySR;
+    private bool checkLight = false;
+	// Use this for initialization
+	void Start () {
+        citySR = GetComponent<SpriteRenderer>();
+        citySR.sprite = unlit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(litFam && !checkLight)
+        {
+            checkLight = true;
+            citySR.sprite = lit;
+        }
+        if(!litFam && checkLight)
+        {
+            checkLight = false;
+            citySR.sprite = unlit;
+        }
+	}
+}
