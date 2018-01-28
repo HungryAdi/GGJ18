5e628384ea61e01469ace3b86f3184e0995b445e
arakij <arakij@uci.edu>
2018-01-28 14:48:08 -0800

    Added to quinns scene

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -9,27 +9,37 @@ public class Platform : PowerUser {
     bool switchPressed;
     Vector3 origin;
     float timer;
+    SpriteRenderer sr;
+    Collider2D col;
+    bool wasPressed;
 
     // Use this for initialization
     protected override void Start() {
         base.Start();
         origin = platform.transform.position;
+        sr = platform.GetComponent<SpriteRenderer>();
+        col = platform.GetComponent<Collider2D>();
+        sr.enabled = false;
+        col.enabled = false;
         timer = 0;
     }
 
     // Update is called once per frame
     void Update() {
-        if (switchPressed) {
-            if (referencePoint.transform.position.y == platform.transform.position.y) {
-                platform.transform.position += Vector3.right * Mathf.Sin(timer) * (referencePoint.transform.position.x - origin.x) * Time.deltaTime;
-                rb.MovePosition(platform.transform.position + Vector3.right * Mathf.Sin(timer) * (referencePoint.transform.position.x - origin.x) * Time.deltaTime);
-                timer += Time.deltaTime;
-            } else if (referencePoint.transform.position.x == platform.transform.position.x) {
-                platform.transform.position += Vector3.up * Mathf.Sin(timer) * (referencePoint.transform.position.y - origin.y) * Time.deltaTime;
-                rb.MovePosition(platform.transform.position += Vector3.up * Mathf.Sin(timer) * (referencePoint.transform.position.y - origin.y) * Time.deltaTime);
-                timer += Time.deltaTime;
-            }
+        if (switchPressed && !wasPressed) {
+            sr.enabled = true;
+            col.enabled = true;
+            //if (referencePoint.transform.position.y == platform.transform.position.y) {
+            //    platform.transform.position += Vector3.right * Mathf.Sin(timer) * (referencePoint.transform.position.x - origin.x) * Time.deltaTime;
+            //    rb.MovePosition(platform.transform.position + Vector3.right * Mathf.Sin(timer) * (referencePoint.transform.position.x - origin.x) * Time.deltaTime);
+            //    timer += Time.deltaTime;
+            //} else if (referencePoint.transform.position.x == platform.transform.position.x) {
+            //    platform.transform.position += Vector3.up * Mathf.Sin(timer) * (referencePoint.transform.position.y - origin.y) * Time.deltaTime;
+            //    rb.MovePosition(platform.transform.position += Vector3.up * Mathf.Sin(timer) * (referencePoint.transform.position.y - origin.y) * Time.deltaTime);
+            //    timer += Time.deltaTime;
+            //}
         }
+        wasPressed = switchPressed;
     }
 
     public override void SetPower(bool isPowered) {
