6adb7b4edb2ff87203fad81a07b97ab14a433e20
arakij <arakij@uci.edu>
2018-01-28 14:10:22 -0800

    More stuff

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,7 +6,9 @@ public class Door : PowerUser {
 
     public SpriteRenderer sr;
     public Collider2D col;
+    public bool zoom;
     bool open;
+    bool wasOpen;
 
     // Use this for initialization
     protected override void Start() {
@@ -15,10 +17,14 @@ public class Door : PowerUser {
 
     // Update is called once per frame
     void Update() {
-        if (open) {
+        if (open && !wasOpen) {
             sr.enabled = false;
             col.enabled = false;
+            if (zoom) {
+                StartCoroutine(Camera.main.GetComponent<camerastuff>().ZoomOutTime(5));
+            }
         }
+        wasOpen = open;
     }
 
     public override void SetPower(bool isPowered) {
